35a09c9bdbee8bc5107d7557788434c0d82ad0e4
edenyav <edenyav@post.bgu.ac.il>
2021-05-28 20:20:36 +0300

    Favorite games table updatad

--- sql_scripts/INSERT INTO Users.sql
+++ sql_scripts/INSERT INTO Users.sql
@@ -1,19 +1,31 @@
 INSERT INTO Users
    ([username],[password],[firstname],[lastname],
-   [country],[email],[imageurl],[FavoritePlayers],[FavoriteTeams])
+   [country],[email],[imageurl])
    
 VALUES
    ('eden6', 'red', 'eden','yav','iraq','ede@.com',
-   'dsfsfs.com',5,4)
+   'dsfsfs.com'),
+     ('guy1', '123', 'guy','shimi','iraq','guy@.com',
+   'examp.com')
 
 GO
 -- Query the total count of employees
 SELECT * FROM dbo.Users;
 -- Query all employee information
 GO
-INSERT INTO Users ([FavoritePlayers]) 
+INSERT INTO UsersFavoritePlayers ([username], [playerid])
+VALUES ('eden6',172104), ('eden6',5627), ('guy1',5627) 
+SELECT * FROM dbo.UsersFavoritePlayers;
 
-VALUES 
-    (7)
 
-WHERE username = 'eden2';
+INSERT INTO UsersFavoriteTeams ([username], [teamid])
+VALUES ('eden6',939), ('guy1',939) 
+SELECT * FROM dbo.UsersFavoritePlayers;
+GO
+
+INSERT INTO UsersFavoriteGames ([username], [GameDateTime],
+[HomeTeam], [AwayTeam])
+VALUES ('eden6','2021-04-03 19:00:00', 'FC Barcelona', 'Macabi Haifa'),
+ ('guy1','2021-04-04 19:00:00', 'FC Barcelona', 'Hapoel Beersheba') 
+SELECT * FROM dbo.UsersFavoritePlayers;
+GO
