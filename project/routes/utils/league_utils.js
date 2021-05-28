7f70c8d47d23003701fe27bf2fbc18844920552d
edenyav <edenyav@post.bgu.ac.il>
2021-05-28 19:25:03 +0300

    next game functionality added

--- project/routes/utils/league_utils.js
+++ project/routes/utils/league_utils.js
@@ -1,7 +1,20 @@
 const axios = require("axios");
+const DButils = require(".\\DButils");
+const app_utils = require(".\\app_utils");
 const LEAGUE_ID = 271;
 
+async function getNextGame(){
+  const now = app_utils.formatDateTime(new Date())
+  const next_games = await DButils.execQuery(`SELECT * From Games WHERE GameDateTime >  '${now}' ORDER BY GameDateTime`)
+  let next_game = next_games[0]
+  next_game['GameDateTime'] = app_utils.formatDateTime(next_game['GameDateTime'])
+  return next_game
+}
+
 async function getLeagueDetails() {
+  
+
+
   const league = await axios.get(
     `https://soccer.sportmonks.com/api/v2.0/leagues/${LEAGUE_ID}`,
     {
@@ -20,12 +33,16 @@ async function getLeagueDetails() {
         api_token: process.env.api_token,
       },
     }
-  );
+  );  
+
+  const next_game = await getNextGame();
+
   return {
     league_name: league.data.data.name,
     current_season_name: league.data.data.season.data.name,
     current_stage_name: stage.data.data.name,
-    // next game details should come from DB
+    stage_next_game: next_game
   };
 }
+
 exports.getLeagueDetails = getLeagueDetails;
