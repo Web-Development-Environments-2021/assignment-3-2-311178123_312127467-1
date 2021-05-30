e735ccbd41486d5dc3ebe35b3afc0fe3c8b734c2
edenyav <edenyav@post.bgu.ac.il>
2021-05-30 22:15:10 +0300

    Update insert with new games

--- sql_scripts/INSERT INTO GAMES EVENTS.sql
+++ sql_scripts/INSERT INTO GAMES EVENTS.sql
@@ -3,11 +3,17 @@ INSERT INTO GamesEvents
    ([gameid],[EventGameTime],[Event])
    
 VALUES
-   (1, 10, 'Goal Ruven Atar'),
-   (1, 44, 'Goal Ruven Atar'),
-    (1, 91, 'Goal Ruven Atar'),
-    (2, 73, 'Red Card Messi'),
-    (2, 90,'Goal Ben Sahar')
+   (1, 10, 'Goal Mikkel Anderson'),
+   (1, 44, 'Goal Mikkel Anderson'),
+   (1, 91, 'Goal Mikkel Anderson'),
+   (2, 73, 'Red  Mikkel Anderson'),
+   (2, 85,'Goal Ster Grytebustg'),
+   (3, 13,'Goal Ster Grytebustg'),
+   (4, 21,'Yellow Card Quentin Westberg'),
+   (6, 17,'Yellow Card Quentin Westberg'),
+   (6, 46,'Goal Quentin Westberg'),
+   (6, 62,'Goal Lie Zhang'),
+   (6, 78,'Goal Lie Zhang')
 GO
 
 Select * FROM GamesEvents;
--- sql_scripts/INSERT INTO GAMES.sql
+++ sql_scripts/INSERT INTO GAMES.sql
@@ -5,13 +5,24 @@ INSERT INTO Games
    [AwayTeamID], [Stadium],[Result], [Referee])
    
 VALUES
-   ('2021-04-03 19:00:00', 'Midtjylland', 939, 'København',85, 'Kiryat Eliezer', 
+   ('2021-01-03 19:00:00', 'Midtjylland', 939, 'København',85, 'MCH Arena', 
    '0-3', 'Daiyrbek Abdyldayev'),
-   ('2021-04-04 19:00:00', 'Midtjylland',939, 'København',85, 'Bernabeu', 
+   ('2021-01-10 19:00:00', 'Midtjylland',939, 'København',85, 'MCH Arena', 
    '0-1','Zainiddin Alimov'),
-    ('2021-08-01 19:00:00', 'Midtjylland', 939, 'København',85, 'Bernabeu', NULL,'Denis Shalayev'),
-    ('2021-08-03 19:00:00', 'Midtjylland', 939, 'København',85, 'Old Trafford', NULL,NULL)
-
+   ('2021-01-13 19:00:00', 'Toronto',111, 'København',85, 'BMO Field', 
+   '0-1','Daiyrbek Abdyldayev'),
+   ('2021-01-20 19:00:00', 'Midtjylland',939, 'Toronto',111, 'MCH Arena', 
+   '0-0','Denis Shalayev'),
+   ('2021-01-13 19:00:00', 'Toronto',111, 'Beijing Renhe',85, 'BMO Field', 
+   '0-0','Daiyrbek Abdyldayev'),
+   ('2021-02-03 19:00:00', 'Toronto',111, 'Beijing Renhe',111, 'BMO Field', 
+   '1-2','Zainiddin Alimov'),
+    ('2021-08-01 19:00:00', 'København', 85, 'Midtjylland',939, 'Parken', NULL,'Denis Shalayev'),
+    ('2021-08-03 19:00:00', 'Toronto', 111, 'Midtjylland',939, 'BMO Field', NULL,'Denis Shalayev'),
+     ('2021-08-10 19:00:00', 'Midtjylland', 939, 'Toronto',111, 'MCH Arena', NULL,NULL),
+    ('2021-10-01 14:30:00', 'København', 939, 'Toronto',111, 'Parken', NULL,NULL),
+    ('2021-10-03 21:05:00', 'København', 939, 'Beijing Renhe',200, 'Parken', NULL,NULL),
+   ('2021-11-05 21:05:00', 'København', 939, 'Beijing Renhe',200, 'Parken', NULL,NULL)
 GO 
 
 
