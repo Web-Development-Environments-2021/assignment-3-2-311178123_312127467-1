5fb36f385df7bcb401495a516fcf2b6f170279e0
edenyav <edenyav@post.bgu.ac.il>
2021-05-31 15:37:34 +0300

    Fix 3 hours behind bug in datetime

--- project/routes/utils/app_utils.js
+++ project/routes/utils/app_utils.js
@@ -2,5 +2,7 @@
 The method will format datetime as YY-MM-DD HH:MM:SS.nano
 */
 exports.formatDateTime = function formatDateTime(dateObj){
-    return dateObj.toISOString().replace("Z","").replace("T", " ")
+    const date = dateObj.toISOString().split("T")[0];
+    const time = `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`
+    return `${date} ${time}`
 }
