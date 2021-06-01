e13943bc407ca840e74769737e31a0c26607511c
edenyav <edenyav@post.bgu.ac.il>
2021-06-01 10:25:28 +0300

    Refactor coach

--- project/routes/utils/coach_utils.js
+++ project/routes/utils/coach_utils.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 const api_domain = "https://soccer.sportmonks.com/api/v2.0";
 
 
+/*
+The method extract all the relevent info about the coach 
+*/
 function extractCoachData(coach_info){
   const { fullname, image_path, nationality } = coach_info;
   return {
@@ -11,6 +14,9 @@ function extractCoachData(coach_info){
   };
 }
 
+/*
+Used for coach personal page - get all the info about the coach from the SportMonk API
+*/
 function getCoachInfo(coachId){
   coach_info = axios.get(`${api_domain}/coaches/${coachId}`, {
     params: {
@@ -22,36 +28,11 @@ function getCoachInfo(coachId){
   };
 }
 
-// function getCoachData(coachId) {
-//   return players_info.map((player_info) => {
-//     const { fullname, image_path, nationality } = player_info.data.data;
-//     const { name } = player_info.data.data.team.data;
-//     return {
-//       name: fullname,
-//       image: image_path,
-//       position: position_id,
-//       team_name: name,
-//     };
-//   });
-// }
 
-async function getCoachIdByTeam(team_id) {
-  const team = await axios.get(`${api_domain}/teams/${team_id}`, {
-    params: {
-      include: "coach",
-      api_token: process.env.api_token,
-    },
-  });
-  coach_id = team.data.data.coach.data.coach_id;
-  return coach_id;
-}
 
-async function getCoachByTeam(team_id) {
-  let coachId = await getCoachIdByTeam(team_id);
-  let coach_info = await getCoachInfo(coachId);
-  return coach_info;
-}
+
 
 exports.extractCoachData = extractCoachData;
+exports.getCoachInfo = getCoachInfo;
 
 
