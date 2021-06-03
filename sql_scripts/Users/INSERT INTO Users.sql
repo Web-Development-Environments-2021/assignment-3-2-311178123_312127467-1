a62724483d9659981c05480e6c7666673d70da1b
edenyav <edenyav@post.bgu.ac.il>
2021-06-03 10:34:07 +0300

    Update inset to users

--- sql_scripts/Users/INSERT INTO Users.sql
+++ sql_scripts/Users/INSERT INTO Users.sql
@@ -1,19 +1,6 @@
 INSERT INTO Users
-   ([username],[password],[firstname],[lastname],
-   [country],[email],[imageurl],[FavoritePlayers],[FavoriteTeams])
+   ([username],[firstname],[lastname],
+   [country],[password],[email],[imageurl])
    
 VALUES
-   ('eden6', 'red', 'eden','yav','iraq','ede@.com',
-   'dsfsfs.com',5,4)
-
-GO
--- Query the total count of employees
-SELECT * FROM dbo.Users;
--- Query all employee information
-GO
-INSERT INTO Users ([FavoritePlayers]) 
-
-VALUES 
-    (7)
-`
-WHERE username = 'eden2';
+   ('edenY', 'eden', 'yav','iraq','1234eden','ede@.com','eden@example.com')
