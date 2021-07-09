0c6f5ab925efa8653f0c664aaaec7f44d213ebeb
edenyav <edenyav@post.bgu.ac.il>
2021-07-09 12:53:32 +0300

    refactor get names

--- project/routes/league.js
+++ project/routes/league.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const league_utils = require("./utils/league_utils");
 
-router.get("/getDetails", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     //const league_details = await league_utils.getLeagueDetails();
     const test_data = {
