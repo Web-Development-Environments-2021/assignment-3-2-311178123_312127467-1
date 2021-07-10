7eb074127b44cb0c1f5e76c78fa548b64a5a5ef2
edenyav <edenyav@post.bgu.ac.il>
2021-07-10 09:38:07 +0300

    add test data

--- project/routes/teams.js
+++ project/routes/teams.js
@@ -42,7 +42,8 @@ router.get("/", async (req, res, next) => {
 
 router.get("/id/:teamId", async (req, res, next) => {
   try {
-    const team_details = await getTeamData(req.params.teamId)
+    // const team_details = await getTeamData(req.params.teamId)
+    const team_details = teams_utils.test_data
     res.send(team_details);
   } catch (error) {
     error.status = 400;
